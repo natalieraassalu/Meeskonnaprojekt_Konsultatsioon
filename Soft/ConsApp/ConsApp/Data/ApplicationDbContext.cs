1964693b7d91d384063e46632922d42ebe692e29
Erik Mägi <erimag@taltech.ee>
2026-05-13 22:39:44 +0300

    Scaffolding tehtud, probleemiks oli entity klassides olevad Guid ja byte andmetüübid, mida scaffolder ei oska töödelda.

--- Soft/ConsApp/ConsApp/Data/ApplicationDbContext.cs
+++ Soft/ConsApp/ConsApp/Data/ApplicationDbContext.cs
@@ -11,5 +11,13 @@ namespace ConsApp.Data
         public DbSet<Abc.Data.Consultation.User> User { get; set; }
         public DbSet<Abc.Data.Consultation.CourseSelector> CourseSelector { get; set; }
         public DbSet<Abc.Data.Consultation.Feedback> Feedback { get; set; }
+        public DbSet<Abc.Data.Consultation.Lecturer> Lecturer { get; set; }
+        public DbSet<Abc.Data.Consultation.Material> Material { get; set; }
+        public DbSet<Abc.Data.Consultation.Notification> Notification { get; set; }
+        public DbSet<Abc.Data.Consultation.BookingPage> BookingPage { get; set; }
+        public DbSet<Abc.Data.Consultation.ConsultationSlot> ConsultationSlot { get; set; }
+        public DbSet<Abc.Data.Consultation.Course> Course { get; set; }
+        public DbSet<Abc.Data.Consultation.CourseConsultation> CourseConsultation { get; set; }
+        public DbSet<Abc.Data.Consultation.CourseMaterial> CourseMaterial { get; set; }
     }
 }
