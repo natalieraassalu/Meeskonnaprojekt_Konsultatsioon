93c4e302bd2164509a6d10a50354bac5b0a04eb1
Natalie Sophie Raassalu <naraas@taltech.ee>
2026-05-14 19:06:03 +0300

    lisasin oma klassidele propertied

--- Code/Data/Consultation/Course.cs
+++ Code/Data/Consultation/Course.cs
@@ -1,7 +1,17 @@
-﻿using Abc.Data.Common;
+﻿using Abc.Aids;
+using Abc.Data.Common;
+using System.ComponentModel.DataAnnotations;
 
 namespace Abc.Data.Consultation;
 
 public sealed class Course : NamedEntity
 {
+    [Random(6, 8, "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789")] public string Code { get; set; } = "";
+    [Random(20, 100)] public string Details { get; set; } = "";      
+
+    public DateTime ValidFrom { get; set; }        
+    public DateTime ValidTo { get; set; }
+    public ICollection<CourseMaterial> CourseMaterials { get; set; } = [];
+    public ICollection<Material> Materials => [.. CourseMaterials.Select(cm => cm.Material)];
+    [Timestamp] public byte[] Timestamp { get; set; } = [];
 }
--- Code/Data/Consultation/Material.cs
+++ Code/Data/Consultation/Material.cs
@@ -1,7 +1,15 @@
-﻿using Abc.Data.Common;
+﻿using Abc.Aids;
+using Abc.Data.Common;
+using System.ComponentModel.DataAnnotations;
 
 namespace Abc.Data.Consultation;
 
 public sealed class Material : NamedEntity
-{
+{ 
+    [Random(3, 5, "0123456789")] public string Number { get; set; } = "";
+    [Random(10, 30)] public string Author { get; set; } = "";
+    [Random(20, 50)] public string Details { get; set; } = "";
+    public ICollection<CourseMaterial> CourseMaterials { get; set; } = [];
+    public ICollection<Course> Courses => [.. CourseMaterials.Select(cm => cm.Course)];
+    [Timestamp] public byte[] Timestamp { get; set; } = [];
 }
