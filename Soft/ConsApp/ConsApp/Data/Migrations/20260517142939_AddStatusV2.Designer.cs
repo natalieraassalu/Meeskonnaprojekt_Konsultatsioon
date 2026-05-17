47ba2296618b6e08109d70c19078ac1f66576e10
Erik Mägi <erimag@taltech.ee>
2026-05-17 17:30:54 +0300

    status klass oli ühelt scaffoldilt puudu nii, et kustutasin vanad scaffoldi, tegin uue status muutujaga ning uue migratsiooni ka

--- Soft/ConsApp/ConsApp/Data/Migrations/20260517142939_AddStatusV2.Designer.cs
+++ Soft/ConsApp/ConsApp/Data/Migrations/20260517142939_AddStatusV2.Designer.cs
@@ -4,6 +4,7 @@ using ConsApp.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace ConsApp.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260517142939_AddStatusV2")]
+    partial class AddStatusV2
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Soft/ConsApp/ConsApp/Data/Migrations/20260517142939_AddStatusV2.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ConsApp.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddStatusV2 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Status",
+                table: "BookingPage");
+
+            migrationBuilder.AddColumn<Guid>(
+                name: "StatusId",
+                table: "ConsultationSlot",
+                type: "uniqueidentifier",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"));
+
+            migrationBuilder.AddColumn<Guid>(
+                name: "StatusId",
+                table: "BookingPage",
+                type: "uniqueidentifier",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"));
+
+            migrationBuilder.CreateTable(
+                name: "Statuses",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    ValidFrom = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    ValidTo = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    Details = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Code = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Statuses", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Statuses");
+
+            migrationBuilder.DropColumn(
+                name: "StatusId",
+                table: "ConsultationSlot");
+
+            migrationBuilder.DropColumn(
+                name: "StatusId",
+                table: "BookingPage");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Status",
+                table: "BookingPage",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+    }
+}
