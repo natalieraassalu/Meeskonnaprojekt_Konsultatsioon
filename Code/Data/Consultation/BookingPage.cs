05551020359618d38d93100de878a0ca1b2984be
romeoristimae <romeor@taltech.ee>
2026-06-01 03:35:49 +0300

    kursuse nimi muutub oigeks ennem naitas teise kursuse nime

--- Code/Data/Consultation/BookingPage.cs
+++ Code/Data/Consultation/BookingPage.cs
@@ -6,10 +6,12 @@ public sealed class BookingPage : BaseEntity
 {
     public DateTime BookingDate { get; set; }
     public string Note { get; set; } = string.Empty;
+    public Guid? CourseId { get; set; }
     public Guid SlotId { get; set; }
     public string Status { get; set; } = string.Empty;
     public Guid StudentId { get; set; }
 
+    public Course? Course { get; set; }
     public ConsultationSlot? Slot { get; set; }
     public User? Student { get; set; }
 }
