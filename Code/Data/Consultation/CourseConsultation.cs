8a9feb1254b523ff9ed802035dcdfdee6ecae3f5
romeoristimae <romeor@taltech.ee>
2026-06-02 22:15:59 +0300

    klassi diagrammi viimane muudatus

--- Code/Data/Consultation/CourseConsultation.cs
+++ Code/Data/Consultation/CourseConsultation.cs
@@ -1,10 +1,13 @@
-﻿using Abc.Data.Common;
+using Abc.Data.Common;
 using System;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Abc.Data.Consultation;
 
-public class CourseConsultation : BaseEntity
-{
+public class CourseConsultation : BaseEntity {
     public Guid CourseId { get; set; }
     public Guid ConsultationSlotId { get; set; }
+
+    [NotMapped] public Course? Course { get; set; }
+    [NotMapped] public ConsultationSlot? ConsultationSlot { get; set; }
 }
