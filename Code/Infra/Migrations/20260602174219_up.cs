9723db554321c064aa29b1030e3004cd2baff68d
Natalie Sophie Raassalu <naraas@taltech.ee>
2026-06-02 21:01:08 +0300

    commit

---
+++ Code/Infra/Migrations/20260602174219_up.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Abc.Infra.Migrations
+{
+    /// <inheritdoc />
+    public partial class up : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
